9074a381a4bebf7555113a1b8a11f06da5a02f3d
andrew webber (personal) <andrewvwebber@googlemail.com>
2022-01-14 16:02:18 +0100

    axum 0.4.0

--- src/binding/axum/response.rs
+++ src/binding/axum/response.rs
@@ -1,22 +1,24 @@
 use axum_lib as axum;
 
-use axum::{body::Body, http::Response, response::IntoResponse};
+use axum::{
+    body::{boxed, BoxBody},
+    http::Response,
+    response::IntoResponse,
+};
 use http::{header, StatusCode};
+use hyper::body::Body;
 
 use super::server_response::event_to_response;
 use crate::event::Event;
 
 impl IntoResponse for Event {
-    type Body = Body;
-    type BodyError = <Self::Body as axum::body::HttpBody>::Error;
-
-    fn into_response(self) -> Response<Body> {
+    fn into_response(self) -> Response<BoxBody> {
         match event_to_response(self) {
             Ok(resp) => resp,
             Err(err) => Response::builder()
                 .status(StatusCode::INTERNAL_SERVER_ERROR)
                 .header(header::CONTENT_TYPE, "text/plain")
-                .body(err.to_string().into())
+                .body(boxed(Body::from(err.to_string())))
                 .unwrap(),
         }
     }
--- src/binding/axum/server_response.rs
+++ src/binding/axum/server_response.rs
@@ -1,6 +1,10 @@
 use axum_lib as axum;
 
-use axum::{body::Body, http::Response};
+use axum::{
+    body::{boxed, BoxBody},
+    http::Response,
+};
+use hyper::body::Body;
 use std::cell::Cell;
 
 use crate::binding::http::{Builder, Serializer};
@@ -11,24 +15,24 @@ struct Adapter {
     builder: Cell<http::response::Builder>,
 }
 
-impl Builder<Response<Body>> for Adapter {
+impl Builder<Response<BoxBody>> for Adapter {
     fn header(&mut self, key: &str, value: http::header::HeaderValue) {
         self.builder.set(self.builder.take().header(key, value));
     }
-    fn body(&mut self, bytes: Vec<u8>) -> Result<Response<Body>> {
+    fn body(&mut self, bytes: Vec<u8>) -> Result<Response<BoxBody>> {
         self.builder
             .take()
-            .body(Body::from(bytes))
+            .body(boxed(Body::from(bytes)))
             .map_err(|e| crate::message::Error::Other {
                 source: Box::new(e),
             })
     }
-    fn finish(&mut self) -> Result<Response<Body>> {
+    fn finish(&mut self) -> Result<Response<BoxBody>> {
         self.body(Vec::new())
     }
 }
 
-pub fn event_to_response(event: Event) -> std::result::Result<Response<Body>, Error> {
+pub fn event_to_response(event: Event) -> std::result::Result<Response<BoxBody>, Error> {
     BinaryDeserializer::deserialize_binary(
         event,
         Serializer::new(Adapter {
